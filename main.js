965d165f0d8e9f350cfaaf702cf5b5c539ab8367
William Swanson <swansontec@gmail.com>
2019-03-24 13:59:18 -0700

    Original version

---
+++ main.js
@@ -0,0 +1,90 @@
+import parse from "csv-parse/lib/sync";
+import stringify from "csv-stringify/lib/sync";
+import { readFileSync, writeFileSync } from "fs";
+import assert from "assert";
+
+const textIn = readFileSync("./sfox-transactions.csv", "utf8");
+const rowsIn = parse(textIn, {
+  columns: true,
+  skip_empty_lines: true
+});
+const tradesIn = rowsIn.filter(
+  row => row.Action === "Buy" || row.Action === "Sell"
+);
+
+// Length should be even:
+assert((tradesIn.length & 1) === 0);
+
+// Translate the trades into the new format:
+const rowsOut = [
+  [
+    "Date",
+    "Source",
+    "Action",
+    "Symbol",
+    "Volume",
+    "Currency",
+    "Price",
+    "Fee",
+    "FeeCurrency"
+  ]
+];
+for (let i = 0; i < tradesIn.length; i += 2) {
+  assert(tradesIn[i].Date === tradesIn[i + 1].Date);
+  assert(tradesIn[i].Price === tradesIn[i + 1].Price);
+  assert(tradesIn[i].Fees === tradesIn[i + 1].Fees);
+
+  const date = tradesIn[i].Date.replace("T", " ").replace(".000Z", " Z");
+  const source = "SFOX";
+  const fee = tradesIn[i].Fees;
+  const price = tradesIn[i].Price;
+  const feeCurrency = "USD";
+
+  // Figure out which row is which:
+  const from = tradesIn[i].Quantity < 0 ? i : i + 1;
+  const to = tradesIn[i].Quantity < 0 ? i + 1 : i;
+
+  const fromCoin = tradesIn[from]["Source Currency"].toUpperCase();
+  const toCoin = tradesIn[to]["Source Currency"].toUpperCase();
+
+  if (toCoin === "USD") {
+    const action = "SELL";
+    const symbol = fromCoin
+    const volume = -tradesIn[from]["Quantity"];
+    const currency = toCoin
+
+    rowsOut.push([
+      date,
+      source,
+      action,
+      symbol,
+      volume,
+      currency,
+      price,
+      fee,
+      feeCurrency
+    ]);
+  } else {
+    const action = "BUY";
+    const symbol = toCoin
+    const volume = tradesIn[to]["Quantity"];
+    const currency = fromCoin
+  
+    rowsOut.push([
+      date,
+      source,
+      action,
+      symbol,
+      volume,
+      currency,
+      price,
+      fee,
+      feeCurrency
+    ]);
+  }
+
+}
+
+const outText = stringify(rowsOut);
+console.log(outText);
+writeFileSync("./sfox-fixed.csv", outText, "utf8");
